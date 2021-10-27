e86bfab78e0262b7c2594c679f2aa08810f5d0e6
Anthony clark <clarkanthony474@yahoo.com>
2021-10-27 12:41:09 -0400

    first attempt at running a function

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 ''' 
-date: xx/xx/xxxx
-author: kjhgdhdj
-description: heres a description of what the function does
+Authonr(s): Anthony, Nick, Dylan
+Created date: 10/27/21
+Description: This is the main class for checkpy, this is what the users will interact with whn the file is run.
+last edit: 10/27/21
+PLEASE UPDATE LAST EDIT WHEN YOU UPDATE THIS FILE
 '''
+
+
+''' 
+date: 10/27/21
+author: Anthony
+description: This is the main class of the file.
+'''
+import modes
+
+class main: 
+    modes.Tools.auto8(input("enter a filename"))
+    pass
+
+
+if __name__ == "__main__":
+    main()
