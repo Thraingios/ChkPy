ac7d8b3c61cfaad2b22e0995ee69d2dd0ced9163
nwc1020 <nwc1020@wildcats.unh.edu>
2021-11-02 13:51:37 -0400

    totally forgot to push this earlier, but added my method as well as cleaned up spelling and made the input universal for all of us for now

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-''' 
+'''
 Authonr(s): Anthony, Nick, Dylan
 Created date: 10/27/21
 Description: This is the main class for checkpy, this is what the users will interact with whn the file is run.
@@ -7,17 +7,27 @@ PLEASE UPDATE LAST EDIT WHEN YOU UPDATE THIS FILE
 '''
 
 
-''' 
+'''
 date: 10/27/21
 author: Anthony
 description: This is the main class of the file.
 '''
+
+
+
+
 import modes
 import art
-class main: 
+class main:
     art.openart()
-    modes.Tools.auto8(input("ENTER A FILENAME HERE -> ")) #currently useing input for testing ownly
-    pass
+
+    myfile = input("ENTER A FILENAME HERE -> ")
+
+    mypylint = modes.Tools.pylint(myfile)
+    print(mypylint[0])
+
+    # currently using input for testing only, changed this to use input at myfile so i didnt have to repeat myself - Nick
+    modes.Tools.auto8(myfile)
 
 
 if __name__ == "__main__":
