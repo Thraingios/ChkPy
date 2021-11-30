b525e975e66b83d1c8372cec2f06d11a1a233e49
nwc1020 <nwc1020@wildcats.unh.edu>
2021-11-30 18:25:03 -0500

    test cases should be done

--- test.py
+++ test.py
@@ -54,8 +54,81 @@ class TestReport(unittest.TestCase):
         main(['b.py', '-r'])
         log.stop()
         messages = log.ret()
-        print(''.join(messages))
         keys = ['OUTPUT FROM PYLINT!', 'OUTPUT FROM PYCODESTYLE!', 'does not exist']
         self.assertIn(keys[0], messages[0])
         self.assertIn(keys[1], messages[2])
         self.assertIn(keys[2], messages[0])
+
+class TestDev(unittest.TestCase):
+    def test_dev_mode_normal(self):
+        """
+        tests report mode on a single file, best case scenario
+        just testing to see if it at least runs both functions that its supposed to.
+        """
+        log = Logger()
+        log.reset()
+        log.start()
+        main(['filetest.py', '-d'])
+        log.stop()
+        messages = log.ret()
+        keys = ['OUTPUT FROM PYLINT!', 'OUTPUT FROM PYCODESTYLE!','RUNNING AUTOPEP8 WITH AGRESSIVENESS LVL 3']
+        self.assertIn(keys[2], messages[0])
+        self.assertIn(keys[1], messages[4])
+        self.assertIn(keys[0], messages[2])
+        
+
+    def test_dev_mode_wrong_file(self):
+        """
+        tests report mode to see if it runs with a file that doesnt exist, 
+        which while it should run both the functions, should also report back that the file doesnt exist
+        """
+        log= Logger()
+        log.reset()
+        log.start()
+        main(['b.py', '-d'])
+        log.stop()
+        messages = log.ret()
+        keys = ['OUTPUT FROM PYLINT!', 'OUTPUT FROM PYCODESTYLE!','RUNNING AUTOPEP8 WITH AGRESSIVENESS LVL 3', 'does not exist']
+        self.assertIn(keys[2], messages[0])
+        self.assertIn(keys[1], messages[4])
+        self.assertIn(keys[0], messages[2])
+        self.assertIn(keys[3], messages[2])
+
+class TestDefault(unittest.TestCase):
+    def test_default_mode_normal(self):
+        """
+        tests report mode on a single file, best case scenario
+        just testing to see if it at least runs both functions that its supposed to.
+        """
+        log = Logger()
+        log.reset()
+        log.start()
+        main(['filetest.py'])
+        log.stop()
+        messages = log.ret()
+        keys = ['OUTPUT FROM PYLINT!', 'OUTPUT FROM PYCODESTYLE!','RUNNING AUTOPEP8 WITH AGRESSIVENESS LVL 3']
+        self.assertIn(keys[0], messages[0])
+        self.assertIn(keys[1], messages[2])
+        self.assertIn(keys[2], messages[4])
+        self.assertIn(keys[0], messages[6])
+        self.assertIn(keys[1], messages[8])
+        
+
+    def test_default_mode_wrong_file(self):
+        """
+        tests default mode to see if it runs with a file that doesnt exist, 
+        which while it should run both the functions, should also report back that the file doesnt exist
+        """
+        log= Logger()
+        log.reset()
+        log.start()
+        main(['b.py'])
+        log.stop()
+        messages = log.ret()
+        keys = ['OUTPUT FROM PYLINT!', 'OUTPUT FROM PYCODESTYLE!','RUNNING AUTOPEP8 WITH AGRESSIVENESS LVL 3', 'does not exist']
+        self.assertIn(keys[0], messages[0])
+        self.assertIn(keys[3], messages[0])
+        self.assertIn(keys[1], messages[2])
+        self.assertIn(keys[2], messages[4])
+        self.assertIn(keys[0], messages[6])
+        self.assertIn(keys[1], messages[8])
