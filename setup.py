d91eb3140b277dc4d86a4da79d32fcb74df9fc02
Dylan Haus <dylan.haus@unh.edu>
2021-12-07 16:05:03 -0500

    made fully chkpy compliant

--- setup.py
+++ setup.py
@@ -2,21 +2,20 @@
 Author(s): Nick, Dylan
 Created date: 12/3/21
 Description: sets up for pip installation
-last edit: 12/6/21
+last edit: 12/7/21
 '''
 
-import os
 from setuptools import setup
 
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding='UTF-8') as fh:
     long_description = fh.read()
 
 
 # Setting up
 setup(
     name="chkpy",
-    version= '1.0.0',
+    version='1.0.0',
     author="Anthony Clark, Dylan Haus & Nick Cardin",
     author_email="<mail@unh.edu>",
     description='Wrapper for 3 python tools to aid in code development',
@@ -29,7 +28,6 @@ setup(
     keywords=['python', 'wrapper', 'development'],
     entry_points={'console_scripts': ['chkpy = chkpy.__main__:main']},
     classifiers=[
-        "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
